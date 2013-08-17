22ef8c231f13cfdb9f2dd9aa3290b21bd8f993a2
Adam Babik <a.babik@designfortress.com>
2013-08-17 18:41:36 +0200

    fixed the rest resources

--- src/builder.js
+++ src/builder.js
@@ -82,7 +82,77 @@ Builder.prototype.buildRegexp = function (pathFragments, params) {
   return new RegExp('^' + joined + '$', 'i');
 };
 
+/**
+ * Extends this.params by new definitions from config
+ * @private
+ * @param  {object} config new params
+ */
+Builder.prototype.extendParams = function (config) {
+  var id;
 
+  for (id in config) {
+    if (!config.hasOwnProperty(id)) {
+      continue;
+    }
+
+    if (id in this.params) {
+      throw new Error("Param '" + id + "' is duplicated.");
+    }
+
+    this.params[id] = {
+      re: config[id],
+      index: -1
+    };
+  }
+};
+
+Builder.prototype._detailedResource = function (type, path, config, handler) {
+  if (typeof config === 'function') {
+    handler = config;
+    config = {};
+  } else {
+    config || (config = {});
+  }
+
+  if (!_.isString(path)) {
+    throw new Error("'path' argument must be a string");
+  }
+
+  if (!_.isFunction(handler)) {
+    throw new Error("'handler' argument must be a function");
+  }
+
+  this.pathFragments.push(path);
+  this.extendParams(config);
+
+  var res = new Resource(
+    type,
+    this.buildRegexp(this.pathFragments, this.params),
+    _.clone(this.params),
+    handler
+  );
+
+  this.resources.push(res);
+
+  return this;
+};
+
+Builder.prototype._simpleResource = function (type, handler) {
+  if (!_.isFunction(handler)) {
+    throw new Error("'handler' argument must be a function");
+  }
+
+  var res = new Resource(
+    type,
+    this.buildRegexp(this.pathFragments, this.params),
+    _.clone(this.params),
+    handler
+  );
+
+  this.resources.push(res);
+
+  return this;
+};
 
 /**
  * Define new resource
@@ -104,22 +174,10 @@ Builder.prototype.resource = function (path) {
  * @param  {function} handler
  * @return {object}   self
  */
-Builder.prototype.all = function (handler) {
-  if (!_.isFunction(handler)) {
-    throw new Error("'handler' argument must be a function");
-  }
-
-  var res = new Resource(
-    Resource.Type.LIST,
-    this.buildRegexp(this.pathFragments, this.params),
-    _.clone(this.params),
-    handler
-  );
-
-  this.resources.push(res);
-
-  return this;
-};
+Builder.prototype.all = _.partial(
+  Builder.prototype._simpleResource,
+  Resource.Type.LIST
+);
 
 /**
  * Create GET resource
@@ -127,74 +185,19 @@ Builder.prototype.all = function (handler) {
  * @param {object} config
  * @param {function} handler
  */
-Builder.prototype.get = function (path, config, handler) {
-  // config was skipped
-  if (typeof config === 'function') {
-    handler = config;
-    config = {};
-  } else {
-    config || (config = {});
-  }
-
-  if (!_.isString(path)) {
-    throw new Error("'path' argument must be a string");
-  }
-
-  if (!_.isFunction(handler)) {
-    throw new Error("'handler' argument must be a function");
-  }
-
-  this.pathFragments.push(path);
-
-  //@TODO: validate duplicate params
-  for (var id in config) {
-    if (!config.hasOwnProperty(id)) {
-      continue;
-    }
-
-    if (id in this.params) {
-      throw new Error("Param '" + id + "' is duplicated.");
-    }
-
-    this.params[id] = {
-      re: config[id],
-      index: -1
-    };
-  }
-
-  var res = new Resource(
-    Resource.Type.GET,
-    this.buildRegexp(this.pathFragments, this.params),
-    _.clone(this.params),
-    handler
-  );
-
-  this.resources.push(res);
-
-  return this;
-};
+Builder.prototype.get = _.partial(
+  Builder.prototype._detailedResource,
+  Resource.Type.GET
+);
 
 /**
  * Create POST resource
  * @param {function} handler
  */
-Builder.prototype.save = function (handler) {
-  if (!_.isFunction(handler)) {
-    throw new Error("'handler' argument must be a function");
-  }
-
-  var lastResource = _.last(this.resources),
-      res = new Resource(
-        Resource.Type.SAVE,
-        this.buildRegexp(this.pathFragments),
-        lastResource ? lastResource.params : null,
-        handler
-      );
-
-  this.resources.push(res);
-
-  return this;
-};
+Builder.prototype.save = _.partial(
+  Builder.prototype._simpleResource,
+  Resource.Type.SAVE
+);
 
 /**
  * Create PUT resource
@@ -202,36 +205,10 @@ Builder.prototype.save = function (handler) {
  * @param {object} config
  * @param {function} handler
  */
-Builder.prototype.update = function (path, config, handler) {
-  if (typeof config === 'function') {
-    handler = config;
-    config = null;
-  } else {
-    config || (config = null);
-  }
-
-  if (!_.isString(path)) {
-    throw new Error("'path' argument must be a string");
-  }
-
-  if (!_.isFunction(handler)) {
-    throw new Error("'handler' argument must be a function");
-  }
-
-  this.pathFragments = this.pathFragments.concat(this.parsePath(path, config));
-
-  var lastResource = _.last(this.resources),
-      res = new Resource(
-        Resource.Type.UPDATE,
-        this.buildRegexp(this.pathFragments),
-        lastResource ? _.extend({}, lastResource.params, config) : config,
-        handler
-      );
-
-  this.resources.push(res);
-
-  return this;
-};
+Builder.prototype.update = _.partial(
+  Builder.prototype._detailedResource,
+  Resource.Type.UPDATE
+);
 
 /**
  * Create DELETE resource
@@ -239,40 +216,13 @@ Builder.prototype.update = function (path, config, handler) {
  * @param {object} config
  * @param {function} handler
  */
-Builder.prototype.delete = function (path, config, handler) {
-  if (typeof config === 'function') {
-    handler = config;
-    config = null;
-  } else {
-    config || (config = null);
-  }
-
-  if (!_.isString(path)) {
-    throw new Error("'path' argument must be a string");
-  }
-
-  if (!_.isFunction(handler)) {
-    throw new Error("'handler' argument must be a function");
-  }
-
-  this.pathFragments = this.pathFragments.concat(this.parsePath(path, config));
-
-  var lastResource = _.last(this.resources),
-      res = new Resource(
-        Resource.Type.DELETE,
-        this.buildRegexp(this.pathFragments),
-        lastResource ? _.extend({}, lastResource.params, config) : config,
-        handler
-      );
-
-  this.resources.push(res);
-
-  return this;
-};
+Builder.prototype.delete = _.partial(
+  Builder.prototype._detailedResource,
+  Resource.Type.DELETE
+);
 
 /**
  * Create new instance of Builderwith current parh fragments, but empty resources array
- *
  * @return {object} Builder
  */
 Builder.prototype.detach = function () {
@@ -286,7 +236,6 @@ Builder.prototype.detach = function () {
 
 /**
  * Work in the same way like detach but remove the last resource before returning.
- *
  * @return {[type]} [description]
  */
 Builder.prototype.end = function () {
@@ -310,7 +259,6 @@ Builder.prototype.end = function () {
 
 /**
  * Return new instance of api, with no resources.
- *
  * @return {[type]} [description]
  */
 Builder.prototype.begin = function () {
